542901dc4739b88be3ec7d60a9af569ac2a410c2
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-08-03 14:26:03 +0530

    fin:1c

--- JS/1-Asg/1-Pop/script.js
+++ JS/1-Asg/1-Pop/script.js
@@ -5,17 +5,34 @@ function showDate() {
     var yyyy = today.getFullYear();
 
     today = mm + '/' + dd + '/' + yyyy;
-    document.getElementById("date").innerHTML=today;
+    document.getElementById("date").innerHTML = today;
 }
 
-function fact(n){
-    if(n==0){
+function fact(n) {
+    if (n == 0) {
         return 1;
     }
-    return n*fact(n-1);
+    return n * fact(n - 1);
 }
 
-function factorial(){
+function factorial() {
     let n = prompt("Enter a number to find factorial:")
-    alert("Factorial of "+n+" is "+fact(n));
+    alert("Factorial of " + n + " is " + fact(n));
+}
+
+
+function showTable() {
+    let n = prompt("Enter a number")
+    alert("Table of " + n + " is:"
+        + "\n" + n + " x 1 = " + n * 1 + "\n" 
+        + n + " x 2 = " + n * 2 + "\n"
+        + n + " x 3 = " + n * 3 + "\n" 
+        + n + " x 4 = " + n * 4 + "\n" 
+        + n + " x 5 = " + n * 5 + "\n" 
+        + n + " x 6 = " + n * 6 + "\n" 
+        + n + " x 7 = " + n * 7 + "\n" 
+        + n + " x 8 = " + n * 8 + "\n" 
+        + n + " x 9 = " + n * 9 + "\n" 
+        + n + " x 10 = " + n * 10
+    )
 }
